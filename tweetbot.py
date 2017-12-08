e2666bea5227fcedced1b5a9e9392b9e59bc651d
TEDECKERMAN <ted.eckerman@gmail.com>
2017-12-08 11:49:49 -0800

    Update tweetbot.py

--- tweetbot.py
+++ tweetbot.py
@@ -10,29 +10,27 @@ sqlite_db = 'bots.db'
 conn = sqlite3.connect(sqlite_db)
 cursor = conn.cursor()
 
-def tweet():
-    cursor.execute('''SELECT * FROM bot_list''')
-    creds = cursor.fetchone()
-    acct = (creds[0])
-    ckey = (creds[2])
-    csecret = (creds[3])
-    akey = (creds[4])
-    asecret = (creds[5])
+def tweet(bot):
+    acct = (bot[0])
+    ckey = (bot[2])
+    csecret = (bot[3])
+    akey = (bot[4])
+    asecret = (bot[5])
     auth = tweepy.OAuthHandler(ckey, csecret)
     auth.set_access_token(akey, asecret)
     api = tweepy.API(auth)
     filename=open(tweetfile,'r')
     f=filename.readlines()
     filename.close()
-
     for line in f:
         api.update_status(status=line)
         print '\nTweeting', line, 'from bot', acct
-        time.sleep(5)
+    time.sleep(2)
 
 def main():
     cursor.execute('''SELECT * from bot_list''')
-    for row in cursor:
-        tweet()
+    for bot in cursor:
+        tweet(bot)
+        time.sleep(2)
 
 main()
