ace0085ce313bc7441369ca733b35cd1ef4d7f32
TEDECKERMAN <ted.eckerman@gmail.com>
2017-12-07 21:19:00 -0800

    Update tweetbot.py

--- tweetbot.py
+++ tweetbot.py
@@ -1,19 +1,39 @@
-import tweepy, time, sys
+#!/usr/bin/env python
+
+import tweepy
+import time
+import sys
+import sqlite3
 
 tweetfile = str(sys.argv[1])
+sqlite_db = 'bots.db'
+conn = sqlite3.connect(sqlite_db)
+cursor = conn.cursor()
 
-CONSUMER_KEY = 'YOUR CONSUMER KEY'
-CONSUMER_SECRET = 'YOUR CONSUMER SECRET KEY'
-ACCESS_KEY = 'YOUR ACCESS KEY'
-ACCESS_SECRET = 'YOUR ACCESS SECRET KEY'
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth)
+def tweet():
+    cursor.execute('''SELECT * FROM bot_list''')
+    creds = cursor.fetchone()
+    acct = (creds[0])
+    ckey = (creds[2])
+    csecret = (creds[3])
+    akey = (creds[4])
+    asecret = (creds[5])
+    auth = tweepy.OAuthHandler(ckey, csecret)
+    auth.set_access_token(akey, asecret)
+    api = tweepy.API(auth)
+    filename=open(tweetfile,'r')
+    f=filename.readlines()
+    filename.close()
 
-filename=open(tweetfile,'r')
-f=filename.readlines()
-filename.close()
+    for line in f:
+        api.update_status(status=line)
+        print '\nTweeting', line, 'from bot', acct
+        time.sleep(5)
 
-for line in f:
-    api.update_status(status=line)
-    time.sleep(900)#Tweet every 15 minutes
+
+def main():
+    cursor.execute('''SELECT * from bot_list''')
+    for row in cursor:
+        tweet()
+
+main()
